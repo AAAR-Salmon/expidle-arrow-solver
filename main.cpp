ac46da03fb15c208ff014dd3f4d31f2bbdec8c72
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2021-12-26 01:56:48 +0900

    Refactor: index -> {row,column} tuple and remove redundant en/decode

--- main.cpp
+++ main.cpp
@@ -7,116 +7,65 @@ using index_t = std::tuple<int, int>;
 using board_t = std::vector<std::vector<int>>;
 
 const int mod = 6;
-const size_t length = 37;
-void dfs(std::vector<int> &board, std::vector<int> &hands, size_t index);
-std::tuple<int, int> decode(size_t index);
-size_t encode(int row, int col);
-void flip_around(std::vector<int> &board, size_t index, int amount);
-template<typename T>
-void print_vector(std::vector<T> vec);
+const size_t board_row = 7, board_col = 7;
 
+void dfs(board_t &board, board_t &hands, index_t index);
+void flip_around(board_t &board, index_t index, int amount);
 index_t next(index_t index);
 bool is_in_board(index_t index);
 void print_board(const board_t board);
 
-int main() {
-  std::vector<int> board(length);
-  std::vector<int> hands(length);
-  for (size_t i = 0; i < length; i++) {
-    std::cout << i << ": ";
-    std::cin >> board[i];
-  }
+const index_t end_index = next({ 6, 6 });
 
-  dfs(board, hands, 0);
+int main() {
+  board_t board(board_row, std::vector<int>(board_col));
+  board_t hands(board_row, std::vector<int>(board_col));
+  for (index_t i = { 0, 0 }; i != end_index; i = next(i)) {
+    const auto [r, c] = i;
+    std::cout << r << " " << c << ": " << std::flush;
+    std::cin >> board[r][c];
+    board[r][c]--;
+  }
+  dfs(board, hands, { 0, 0 });
   return 0;
 }
 
-void dfs(std::vector<int> &board, std::vector<int> &hands, size_t index) {
-  if (index == length) {
-    print_vector(hands);
+void dfs(board_t &board, board_t &hands, index_t index) {
+  if (index == end_index) {
+    print_board(hands);
+    exit(0);
     return;
   }
-  auto [r, c] = decode(index);
+  auto [r, c] = index;
   // 右端辺での枝刈り
-  if (c == 6 && r > 3 && board[encode(r - 1, c - 1)] != board[encode(r - 1, c)]) {
+  if (c == 6 && r > 3 && board[r - 1][c - 1] != board[r - 1][c]) {
     return;
   }
   // 下端辺での枝刈り
-  if (r == 6 && c > 3 && board[encode(r - 1, c - 1)] != board[encode(r, c - 1)]) {
+  if (r == 6 && c > 3 && board[r - 1][c - 1] != board[r][c - 1]) {
     return;
   }
   // 上端、左端辺以外での枝刈り
   if (r != 0 && c != 0) {
     // 左上マスを0にするように動かす
-    const int hand = (mod - board[encode(r - 1, c - 1)]) % mod;
-    hands[index] = hand;
+    const int hand = (mod - board[r - 1][c - 1]) % mod;
+    hands[r][c] = hand;
     flip_around(board, index, hand);
-    dfs(board, hands, index + 1);
+    dfs(board, hands, next(index));
     flip_around(board, index, mod - hand);
   } else {
     for (int hand = 0; hand < mod; hand++) {
-      hands[index] = hand;
+      hands[r][c] = hand;
       flip_around(board, index, hand);
-      dfs(board, hands, index + 1);
+      dfs(board, hands, next(index));
       flip_around(board, index, mod - hand);
     }
   }
 }
 
-std::tuple<int, int> decode(size_t index) {
-  uint row, col;
-
-  if (index < 4) {
-    row = 0;
-    col = index;
-  } else if (index < 9) {
-    row = 1;
-    col = index - 4;
-  } else if (index < 15) {
-    row = 2;
-    col = index - 9;
-  } else if (index < 22) {
-    row = 3;
-    col = index - 15;
-  } else if (index < 28) {
-    row = 4;
-    col = index - 22 + 1;
-  } else if (index < 33) {
-    row = 5;
-    col = index - 28 + 2;
-  } else {
-    row = 6;
-    col = index - 33 + 3;
-  }
-
-  return std::tuple<int, int>(row, col);
-}
-
-size_t encode(int row, int col) {
-  size_t index = -1;
-
-  if (row == 0) {
-    if (0 <= col && col <= 3) index = col;
-  } else if (row == 1) {
-    if (0 <= col && col <= 4) index = col + 4;
-  } else if (row == 2) {
-    if (0 <= col && col <= 5) index = col + 9;
-  } else if (row == 3) {
-    if (0 <= col && col <= 6) index = col + 15;
-  } else if (row == 4) {
-    if (1 <= col && col <= 6) index = col + 22 - 1;
-  } else if (row == 5) {
-    if (2 <= col && col <= 6) index = col + 28 - 2;
-  } else if (row == 6) {
-    if (3 <= col && col <= 6) index = col + 33 - 3;
-  }
-
-  return index;
-}
-
-void flip_around(std::vector<int> &board, size_t index, int amount) {
+void flip_around(board_t &board, index_t index, int amount) {
   // 正体不明だがちゃんと最初だけ初期化処理が走る
-  static std::array<std::tuple<int, int>, 7u> dpos{
+  static std::array<index_t, 7u> dpos{
     dpos[0] = { -1, -1 },
     dpos[1] = { -1, 0 },
     dpos[2] = { 0, -1 },
@@ -125,10 +74,11 @@ void flip_around(std::vector<int> &board, size_t index, int amount) {
     dpos[5] = { 1, 0 },
     dpos[6] = { 1, 1 }
   };
-  auto [r, c] = decode(index);
-  for (const auto &[dr, dc] : dpos) {
-    board[encode(r + dr, c + dc)] += amount;
-    board[encode(r + dr, c + dc)] %= mod;
+  const auto [r, c] = index;
+  for (const auto [dr, dc] : dpos) {
+    if (!is_in_board({ r + dr, c + dc })) continue;
+    board[r + dr][c + dc] += amount;
+    board[r + dr][c + dc] %= mod;
   }
 }
 
